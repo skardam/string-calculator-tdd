a7292501c10e449026de2631d7bf9a313ab3c603
skardam <sushikardam@gmail.com>
2025-03-25 23:08:55 +0530

    Test: Should handle new line as delimiter

--- src/utils/__tests__/stringCalculator.test.js
+++ src/utils/__tests__/stringCalculator.test.js
@@ -13,4 +13,7 @@ describe("String Calculator", () => {
   test("should return sum for multiple numbers", () => {
     expect(add("1,2,3,4,5")).toBe(15);
   });
+  test("should handle new line delimiter", () => {
+    expect(add("1\n2,3")).toBe(6);
+  });
 });

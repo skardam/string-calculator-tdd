0651237b72e39d748a8d4cdae4dadf24b6ac9f4a
skardam <sushikardam@gmail.com>
2025-03-25 23:16:03 +0530

    UI: Add basic calculator component structure

---
+++ src/components/StringCalculator.js
@@ -0,0 +1,13 @@
+import React, { useState } from "react";
+import "./StringCalculator.css";
+
+function StringCalculator() {
+  return (
+    <div className="calculator-container">
+      <h1>String Calculator</h1>
+      <div className="calculator-form">{/* Form will go here */}</div>
+    </div>
+  );
+}
+
+export default StringCalculator;

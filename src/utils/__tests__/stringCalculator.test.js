1a64c7cf280c6c07bead0af10206cdfbe9a95231
skardam <sushikardam@gmail.com>
2025-03-25 23:05:27 +0530

    Test: Single number should return its value

--- src/utils/__tests__/stringCalculator.test.js
+++ src/utils/__tests__/stringCalculator.test.js
@@ -4,4 +4,7 @@ describe("String Calculator", () => {
   test("should return 0 for empty string", () => {
     expect(add("")).toBe(0);
   });
+  test("should return number for single number", () => {
+    expect(add("1")).toBe(1);
+  });
 });

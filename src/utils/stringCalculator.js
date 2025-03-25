b4f538ca0b93ea31f12258fcad274600efb419ed
skardam <sushikardam@gmail.com>
2025-03-25 23:14:12 +0530

    Refactor: Improve code organization and readability

--- src/utils/stringCalculator.js
+++ src/utils/stringCalculator.js
@@ -3,21 +3,34 @@ export function add(numbers) {
     return 0;
   }
 
-  let delimiter = ",";
-  let numbersString = numbers;
+  const { delimiter, numbersString } = parseInput(numbers);
+  const nums = splitNumbers(numbersString, delimiter);
 
-  if (numbers.startsWith("//")) {
-    const delimiterEnd = numbers.indexOf("\n");
-    delimiter = numbers.substring(2, delimiterEnd);
-    numbersString = numbers.substring(delimiterEnd + 1);
+  checkForNegatives(nums);
+
+  return nums.reduce((sum, num) => sum + parseInt(num), 0);
+}
+
+function parseInput(input) {
+  let delimiter = ",";
+  let numbersString = input;
+
+  if (input.startsWith("//")) {
+    const delimiterEnd = input.indexOf("\n");
+    delimiter = input.substring(2, delimiterEnd);
+    numbersString = input.substring(delimiterEnd + 1);
   }
 
-  const nums = numbersString.replace(/\n/g, delimiter).split(delimiter);
+  return { delimiter, numbersString };
+}
 
-  const negatives = nums.filter((num) => parseInt(num) < 0);
+function splitNumbers(numbersString, delimiter) {
+  return numbersString.replace(/\n/g, delimiter).split(delimiter);
+}
+
+function checkForNegatives(numbers) {
+  const negatives = numbers.filter((num) => parseInt(num) < 0);
   if (negatives.length > 0) {
     throw new Error(`negative numbers not allowed ${negatives.join(",")}`);
   }
-
-  return nums.reduce((sum, num) => sum + parseInt(num), 0);
 }
